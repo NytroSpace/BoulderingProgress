cd3ec5ce000fcdcebff6441e29516006ba0c0220
LP7257 <lassipalol@gmail.com>
2026-03-23 20:07:20 +0200

    Route modding window
    title

--- src/main/java/fi/jyu/ohj2/nico/BoulderProgress/model/Route.java
+++ src/main/java/fi/jyu/ohj2/nico/BoulderProgress/model/Route.java
@@ -3,7 +3,9 @@ package fi.jyu.ohj2.nico.BoulderProgress.model;
 /// <summary>
 /// All the fields of the route.
 /// </summary>
-public record Route(String grade, WallType wallType, int attempts, boolean isCompleted, String UUID) {}
+public record Route(String grade, WallType wallType, int attempts, boolean isCompleted, String UUID) {
+
+}
 
 
 
