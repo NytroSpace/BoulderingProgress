d41c6dc1de2a46799c02a7af37df7a797c91e63f
Rannikkoluoto Nico Kristian <nico.k.rannikkoluoto@student.jyu.fi>
2026-04-07 16:07:53 +0300

    Added buttons to main controller

--- src/main/java/fi/jyu/ohj2/nico/BoulderProgress/controller/MainController.java
+++ src/main/java/fi/jyu/ohj2/nico/BoulderProgress/controller/MainController.java
@@ -1,10 +1,25 @@
 package fi.jyu.ohj2.nico.BoulderProgress.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button modButton;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private Button exitButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Write initialization code here
