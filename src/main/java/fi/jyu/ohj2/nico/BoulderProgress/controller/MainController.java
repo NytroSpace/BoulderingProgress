9366c9fc6e82264c608a4b9351f47f9120cec24a
Rannikkoluoto Nico Kristian <nico.k.rannikkoluoto@student.jyu.fi>
2026-04-07 16:25:17 +0300

    Improved main window

--- src/main/java/fi/jyu/ohj2/nico/BoulderProgress/controller/MainController.java
+++ src/main/java/fi/jyu/ohj2/nico/BoulderProgress/controller/MainController.java
@@ -3,7 +3,9 @@ package fi.jyu.ohj2.nico.BoulderProgress.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 
+import javax.swing.table.TableColumn;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,6 +21,18 @@ public class MainController implements Initializable {
 
     @FXML
     private Button exitButton;
+
+    @FXML
+    private TableColumn order;
+
+    @FXML
+    private TableColumn date;
+
+    @FXML
+    private TableColumn hardestGrade;
+
+    @FXML
+    private TableColumn totalClimbs;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
