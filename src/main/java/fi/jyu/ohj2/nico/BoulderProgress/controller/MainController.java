86378c9619fc2be71ae60a758d3adab12d75df37
Rannikkoluoto Nico Kristian <nico.k.rannikkoluoto@student.jyu.fi>
2026-04-07 16:55:32 +0300

    Main controller

--- src/main/java/fi/jyu/ohj2/nico/BoulderProgress/controller/MainController.java
+++ src/main/java/fi/jyu/ohj2/nico/BoulderProgress/controller/MainController.java
@@ -1,9 +1,15 @@
 package fi.jyu.ohj2.nico.BoulderProgress.controller;
 
+import fi.jyu.ohj2.nico.BoulderProgress.model.Session;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import javax.swing.table.TableColumn;
 import java.net.URL;
@@ -11,31 +17,41 @@ import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
     @FXML
-    private Button addButton;
+    private Button AddButton;
 
     @FXML
-    private Button modButton;
+    private Button ModifyButton;
 
     @FXML
-    private Button removeButton;
+    private Button RemoveButton;
 
     @FXML
-    private Button exitButton;
+    private Button ExitButton;
 
     @FXML
-    private TableColumn order;
-
-    @FXML
-    private TableColumn date;
-
-    @FXML
-    private TableColumn hardestGrade;
-
-    @FXML
-    private TableColumn totalClimbs;
+    private TableView<Session> MonthlyTable;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Write initialization code here
+        AddButton.setOnAction(e -> openNewSession("Add Session"));
+    }
+
+
+    private void openNewSession(String windowTitle) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("fi/jyu/ohj2/nico/BoulderProgress/session-edit.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle(windowTitle);
+
+            stage.initModality(Modality.APPLICATION_MODAL);
+
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error loading window");
+        }
     }
 }
